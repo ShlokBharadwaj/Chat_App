33573c88239b7cff2f796492c21a82518b6ce0de
Shlok <shlokbharadwaj7@gmail.com>
2022-01-20 09:47:35 +0530

    Done with client and Chat part

--- src/Chat.java
+++ src/Chat.java
@@ -1,3 +1,28 @@
+import java.io.*;
+
+//With the help of this class we will have differnet types of messages.
 public class Chat{
-    
+
+    //Different types of messages that the client can send
+    //WHOISIN to receive the list of the user connected
+    //MESSAGE to send an ordinary text message
+    //LOGOUT to disconnect from the server
+
+    static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
+    private int type;
+    private String message;
+
+    //constructor
+    Chat(int type, String message){
+        this.type = type;
+        this.message = message;
+    }
+
+    int getType(){
+        return type;
+    }
+
+    String getMessage(){
+        return message;
+    }
 }
